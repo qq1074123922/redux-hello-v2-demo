bad7308e2cd90a82b7bf38e128f2308154560aaf
LEO <1074123922@qq.com>
2017-08-24 15:23:33 +0800

    teo commponents

---
+++ src/Post.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import PostBody from './PostBody'
+import CommentBox from './CommentBox'
+import './post.css'
+class Post extends Component {
+  render(){
+    return(
+      <div className="Post">
+        Post
+        <div className="upper">
+          <PostBody />
+        </div>
+        <div className="bottom">
+          <CommentBox />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Post
